5db0232e0d42347eaf1ee4c61860b24c9c2e339f
georgijsemenov <gerogy.semyonov@nedra.digital>
2025-11-17 17:55:52 +0500

    добавлены тесты на практическое задание 20

---
+++ src/test/java/tests/FakerGenerTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import com.microsoft.playwright.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FakerGenerTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = browser.newPage();
+
+            // Генерация данных
+            Faker faker = new Faker();
+            String name = faker.name().name();
+
+            // Мокирование API
+            page.route("**/dynamic_content", route -> {
+                route.fulfill(new Route.FulfillOptions()
+                        .setBody("<div class='large-10 columns'>" + name + "</div>"));
+            });
+
+            // Запуск теста
+            page.navigate("https://the-internet.herokuapp.com/dynamic_content");
+            assertTrue(page.locator(".large-10.columns").textContent().contains(name));
+
+        }
+    }
+}
